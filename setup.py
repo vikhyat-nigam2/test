268d7e42422424201d1edc77891ca436bf77cd3b
vikhyat-nigam2 <154405096+vikhyat-nigam2@users.noreply.github.com>
2024-01-23 16:34:26 +0530

    Create setup

---
+++ setup.py
@@ -0,0 +1,9 @@
+import setuptools
+
+setuptools.setup(
+    include_package_data=True,
+    name="middleware_gen_ai",
+    version="0.0.1",
+    description="gen_ai middleware module",
+    packages=setuptools.find_packages(),
+)
